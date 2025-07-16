81bf4339a215edecdb0198532ce98dd928f34baa
Tobyaki <115458876+Tobyaki@users.noreply.github.com>
2025-07-16 03:15:41 -0700

    Add files via upload

---
+++ Updated assignment.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Please put in your name");
+string? myName = Console.ReadLine();
+
+int i = 0;
+
+foreach (char c in myName)
+{
+    i++;
+}
+Console.WriteLine("The number of characters in your name is " + i);
+
+
+
+
+/**string myName = "tobechukwu";
+int i = 0;
+
+foreach (char c in myName)
+{
+    i++;
+}
+
+Console.WriteLine("The number of characters is " + i); **/
